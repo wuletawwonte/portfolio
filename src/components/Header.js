3d3bddca8eef644c265d824619e8774a3c57c9c2
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-09-28 20:25:41 +0300

    Add header section template

---
+++ src/components/Header.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <header>
+      <h1>wuletaw</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="#">Home</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  )
+}
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,9 @@
 import React from "react"
+import Header from "../components/Header"
 import './index.scss'
 
 export default function Home() {
-  return <div>Hello world!</div>
+  return <>
+    <Header />
+  </>
 }
