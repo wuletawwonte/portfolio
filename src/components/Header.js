6852c93bec11996a28a523c3f57ae40b392725d5
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-09 15:24:57 +0300

    Add avatar picture

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import myPicture from '../assets/images/me.jpg';
 
 export default function Header() {
   return (
     <header id="header">
       <div class="inner">
-        <a href="#" class="image avatar"><img src="images/avatar.jpg" alt="" /></a>
+        <a href="#" class="image avatar">
+          <img src={myPicture} alt="" />
+        </a>
         <h1><strong>I am Strata</strong>, a super simple<br />
         responsive site template freebie<br />
         crafted by <a href="http://html5up.net">HTML5 UP</a>.</h1>
