a559e652ea927330e5a034dd153ab8bced045437
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-09 16:50:15 +0300

    Add static js files in the static folder

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,10 +4,19 @@ import GetInTouch from "../components/GetInTouch"
 import Header from "../components/Header"
 import Hero from "../components/Hero"
 import RecentWorks from "../components/RecentWorks"
+import { Helmet } from "react-helmet"
 import './index.scss'
 
 export default function Home() {
   return <>
+    <Helmet>
+      <script src="js/jquery.min.js"></script>
+			<script src="js/jquery.poptrox.min.js"></script>
+			<script src="js/browser.min.js"></script>
+			<script src="js/breakpoints.min.js"></script>
+			<script src="js/util.js"></script>
+			<script src="js/main.js"></script>
+    </Helmet>
     <Header />
     <div id="main">
       <Hero />
