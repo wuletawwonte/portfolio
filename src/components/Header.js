d756970bf9bd7b9c7c85e55f73aa44a0cb2423b7
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-10 17:32:52 +0300

    Change avatar image location to static folder

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import myPicture from '../assets/images/me.png';
 
 export default function Header() {
   return (
     <header id="header">
       <div className="inner">
         <a href="#" className="image avatar">
-          <img src={myPicture} alt="" />
+          <img src="images/me.png" alt="wuletaw avatar" />
         </a>
         <h1><strong>I am Wuletaw</strong>, a Full-Stack Engineer <br />
         with a love for Javascript, React, Ruby, <a href='#'>Ruby On Rails</a>, Node and Codeigniter.<br />
--- src/components/RecentWorks.js
+++ src/components/RecentWorks.js
@@ -6,32 +6,32 @@ export default function RecentWorks() {
         <h2>Recent Work</h2>
         <div className="row">
             <article className="col-6 col-12-xsmall work-item">
-                <a href="images/fulls/covidtracker.jpeg" className="image fit thumb"><img src="images/thumbs/covidtracker.jpg" alt="" /></a>
+                <a href="images/fulls/covidtracker.jpeg" className="image fit thumb"><img src="images/thumbs/covidtracker.jpg" alt="project snapshot" /></a>
                 <h3>Magna sed consequat tempus</h3>
                 <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
             </article>
             <article className="col-6 col-12-xsmall work-item">
-                <a href="images/fulls/webdevcourse.jpg" className="image fit thumb"><img src="images/thumbs/webdevcourse.jpg" alt="" /></a>
+                <a href="images/fulls/webdevcourse.jpg" className="image fit thumb"><img src="images/thumbs/webdevcourse.jpg" alt="project snapshot" /></a>
                 <h3>Ultricies lacinia interdum</h3>
                 <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
             </article>
             <article className="col-6 col-12-xsmall work-item">
-                <a href="images/fulls/vehiclereservation.jpg" className="image fit thumb"><img src="images/thumbs/vehiclereservation.jpg" alt="" /></a>
+                <a href="images/fulls/vehiclereservation.jpg" className="image fit thumb"><img src="images/thumbs/vehiclereservation.jpg" alt="project snapshot" /></a>
                 <h3>Tortor metus commodo</h3>
                 <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
             </article>
             <article className="col-6 col-12-xsmall work-item">
-                <a href="images/fulls/amudsr.jpg" className="image fit thumb"><img src="images/thumbs/amudsr.jpg" alt="" /></a>
+                <a href="images/fulls/amudsr.jpg" className="image fit thumb"><img src="images/thumbs/amudsr.jpg" alt="project snapshot" /></a>
                 <h3>Quam neque phasellus</h3>
                 <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
             </article>
             <article className="col-6 col-12-xsmall work-item">
-                <a href="images/fulls/expensetracker.jpg" className="image fit thumb"><img src="images/thumbs/expensetracker.jpg" alt="" /></a>
+                <a href="images/fulls/expensetracker.jpg" className="image fit thumb"><img src="images/thumbs/expensetracker.jpg" alt="project snapshot" /></a>
                 <h3>Nunc enim commodo aliquet</h3>
                 <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
             </article>
             <article className="col-6 col-12-xsmall work-item">
-                <a href="images/fulls/bookstore.jpg" className="image fit thumb"><img src="images/thumbs/bookstore.jpg" alt="" /></a>
+                <a href="images/fulls/bookstore.jpg" className="image fit thumb"><img src="images/thumbs/bookstore.jpg" alt="project snapshot" /></a>
                 <h3>Risus ornare lacinia</h3>
                 <p>Lorem ipsum dolor sit amet nisl sed nullam feugiat.</p>
             </article>
