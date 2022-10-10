be7ce25e508a96bceb453576eade959bc62cbffd
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-10 19:19:47 +0300

    Add href to social links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,13 +5,13 @@ export default function Footer() {
     <footer id="footer">
         <div className="inner">
             <ul className="icons">
-                <li><a href="#" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
-                <li><a href="#" className="icon brands fa-github"><span className="label">Github</span></a></li>
-                <li><a href="#" className="icon brands fa-dribbble"><span className="label">Dribbble</span></a></li>
-                <li><a href="#" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
+                <li><a href="https://www.linkedin.com/in/wuletaw-wonte/" target="__blank" className="icon brands fa-linkedin"><span className="label">LinkedIn</span></a></li>
+                <li><a href="https://twitter.com/wuletaww" target="__blank" className="icon brands fa-twitter"><span className="label">Twitter</span></a></li>
+                <li><a href="https://github.com/wuletawwonte" target="__blank" className="icon brands fa-github"><span className="label">Github</span></a></li>
+                <li><a href="mailto: wuletawwonte@gmail.com" className="icon solid fa-envelope"><span className="label">Email</span></a></li>
             </ul>
             <ul className="copyright">
-                <li>&copy; Untitled</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+                <li>&copy; Wuletaw</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
             </ul>
         </div>
     </footer>
