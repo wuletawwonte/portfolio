9eb8344cce61e35debb8666ebb0ab88c19e911ac
Wuletaw Wonte <wuletawwonte@gmail.com>
2022-10-10 18:38:30 +0300

    Remove fonwawesome cdn and use local fontawesome asset files

--- src/pages/index.js
+++ src/pages/index.js
@@ -10,13 +10,12 @@ import './index.scss'
 export default function Home() {
   return <>
     <Helmet>
-      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css" rel="stylesheet" />
-      <script src="js/jquery.min.js"></script>
-			<script src="js/jquery.poptrox.min.js"></script>
-			<script src="js/browser.min.js"></script>
-			<script src="js/breakpoints.min.js"></script>
-			<script src="js/util.js"></script>
-			<script src="js/main.js"></script>
+      <script src="js/jquery.min.js" defer></script>
+			<script src="js/jquery.poptrox.min.js" defer></script>
+			<script src="js/browser.min.js" defer></script>
+			<script src="js/breakpoints.min.js" defer></script>
+			<script src="js/util.js" defer></script>
+			<script src="js/main.js" defer></script>
     </Helmet>
     <Header />
     <div id="main">
